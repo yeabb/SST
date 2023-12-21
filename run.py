cdd506cdef305a19653a42bb47a298b9f1962804
Yeabkal Biru <yeabkalbiru@gmail.com>
2023-12-20 16:51:44 -0800

    added converter and  utility functions for run file

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 
 import argparse
+from run_utils import *
 from norfair import Tracker, Video
-from inference.yolov5 import YoloV5
+from inference import Converter, YoloV5
 
 
 parser = argparse.ArgumentParser()
@@ -11,18 +12,28 @@ parser.add_argument(
     type=str,
     help="Path to the input video",
 )
+parser.add_argument(
+    "--model", default="models/ball.pt", type=str, help="Path to the model"
+)
 
 args = parser.parse_args()
 
 video = Video(input_path=args.video)
 
 player_detector = YoloV5()
+ball_detector = YoloV5(model_path=args.model)
 
 
+j=0
 for i, frame in enumerate(video):
-    person_df = player_detector.predict(frame)
-    person_df = person_df[person_df["name"] == "person"]
-    person_df = person_df[person_df["confidence"] > 0.35]
-    print(person_df)
+    if j==1:
+        break
+    players_detections = get_player_detections(player_detector, frame)
+    ball_detections = get_ball_detections(ball_detector, frame)
+    detections = ball_detections + players_detections
+    
+    for detection in detections:
+        print(detection)
+    j+=1
 
     
